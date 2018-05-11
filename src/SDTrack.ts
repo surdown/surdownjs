3d3357d6d7fe5522a364f66ffe928fa775844cb1
Rajat Talwar <cuterajat26@gmail.com>
2018-05-11 17:20:29 +0530

    fix duration addition when calculating tie over a ba

--- src/SDTrack.ts
+++ src/SDTrack.ts
@@ -3,59 +3,58 @@ import ToneFactory from './ToneFactory';
 import SDNote from './SDNote';
 import { SDBar } from './SDBar';
 
-export default class SDTrack{
-    private _timeLine:SDTimeLine
-    private _inst:any
-    private _root_midi:number
-    private  _tonejs:any
-    
-    constructor(tonejs:any,timeLine:SDTimeLine,inst:any,root_midi:number){
+export default class SDTrack {
+    private _timeLine: SDTimeLine
+    private _inst: any
+    private _root_midi: number
+    private _tonejs: any
+
+    constructor(tonejs: any, timeLine: SDTimeLine, inst: any, root_midi: number) {
         this._timeLine = timeLine;
         this._tonejs = tonejs
         this._inst = inst;
         this._root_midi = root_midi;
         this.recalculate();
-        
+
     }
-    recalculate(){
+    recalculate() {
         let node = this._timeLine.startNode();
         const Tone = this._tonejs;
         let t = Tone.Transport.now() + 0.2;
-        while(node){
+        while (node) {
             let isNote = node instanceof SDNote
             let duration = isNote ? Tone.TimeBase((<SDNote>node).duration()).valueOf() : 0;
-            let triggerValue = isNote ? (t+Tone.TimeBase((<SDNote>node).timeLinePosition().valueOf())) : 0;
+            let triggerValue = isNote ? (t + Tone.TimeBase((<SDNote>node).timeLinePosition().valueOf())) : 0;
             let tieOriginNode = node;
-            while((node.next() instanceof SDBar) || (node.next() && (node.next() instanceof SDNote) && (<SDNote>(node.next())).isTieNote()){
-                    let shouldAddDuration = !(node.next() instanceof SDBar)
-                    duration = duration + (shouldAddDuration ? Tone.TimeBase((<SDNote>node).duration()).valueOf() : 0);
-                
+            while ((node.next() instanceof SDBar) || (node.next() && (node.next() instanceof SDNote) && (<SDNote>(node.next())).isTieNote()){
+                let shouldAddDuration = !(node.next() instanceof SDBar)
+                duration = duration + (shouldAddDuration ? Tone.TimeBase((<SDNote>tieOriginNode).duration()).valueOf() : 0);
                 node = node.next();
             }
-            
-            isNote && this._inst.triggerAttackRelease(new Tone.Frequency(this._root_midi+(<SDNote>tieOriginNode).midiOffset(), "midi"),duration,triggerValue);
+
+            isNote && this._inst.triggerAttackRelease(new Tone.Frequency(this._root_midi + (<SDNote>tieOriginNode).midiOffset(), "midi"), duration, triggerValue);
 
             node = node.next();
         }
     }
-    setTimeLine(){
+    setTimeLine() {
 
     }
-    setInstrument(){
+    setInstrument() {
 
     }
-    play(start?:string){
+    play(start?: string) {
         this.stop();
         this.recalculate();
         this._tonejs.Transport.start(start);
     }
-    restart(){
+    restart() {
 
     }
-    pause(){
+    pause() {
         this._tonejs.Transport.pause();
     }
-    stop(){
+    stop() {
         this._tonejs.Transport.stop();
     }
 }
